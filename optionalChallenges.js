95788d343bf8647a990d7b8433d47ddd9402d46f
Tahmina <tahmina.ringer@gmail.com>
2021-06-05 22:36:57 -0700

    worked through practice challenges

--- optionalChallenges.js
+++ optionalChallenges.js
@@ -18,7 +18,7 @@ function sameFrequency(int1, int2) {
   for (let i = 0; i < strInt1.length; i++) {
     countInt1[strInt1[i]] = (countInt1[strInt1[i]] || 0) + 1;
   }
-  for (let j = 0; j < strInt1.length; j++) {
+  for (let j = 0; j < strInt2.length; j++) {
     countInt2[strInt2[j]] = (countInt2[strInt2[j]] || 0) + 1;
   }
   for (let key in countInt1) {
@@ -68,4 +68,56 @@ function areThereDuplicates(...args) {
 // one liner solution
 function areThereDuplicates() {
   return new Set(arguments).size !== arguments.length;
+}
+
+//Multiple Pointers - averagePair
+//Write a function called averagePair. Given a sorted array of integers and a target average, determine if there is a pair of values in the array where the average of the pair equals the target average. There may be more than one pair that matches the average target. 
+
+function averagePair(arr, num) {
+  let start = 0;
+  let end = arr.length-1;
+  while(start < end) {
+    let avg = (arr[start] + arr[end])/2;
+    if (avg === num) {
+      return true;
+    }
+    else if (avg < num) {
+      start ++;
+    }
+    else {
+      end--;
+    }
+  }
+  return false;
+}
+
+//Multiple Pointers - isSubsequence
+// Write a function called isSubsequence which takes in two strings and checks whether the characters in the first string form a subsequence of the characters in the second string. in other words, the function should check whether the characters in the first string appear somewhere in the second string, without their order changing
+
+//Iterative
+
+function isSubsequence(str1, str2) {
+  var i = 0;
+  var j = 0;
+  if (!str1) {
+    return true;
+  }
+  while (j < str2.length) {
+    if (str2[j] === str1[i]) {
+      i++;
+    }
+    if (i === str1.length) {
+      return true;
+    }
+    j++;
+  }
+  return false;
+}
+
+//Recursive
+function isSubsequnce(str1, str2) {
+  if(str1.length === 0) return true;
+  if(str2.length === 0) return false;
+  if(str2[0] === str1[0]) return isSubsequence(str1.slice(1), str2.slice(1))
+    return isSubsequence(str1, str2.slice(1))
 }
